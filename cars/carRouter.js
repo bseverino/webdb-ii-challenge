3a1dd16b0e86b7402c11d94cdd527124eaa43c9f
B Severino <biancasev@gmail.com>
2020-01-21 15:57:08 -0500

    Created server and router, set up get and post requests

---
+++ cars/carRouter.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const db = require('../data/dbConfig.js')
+
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    db('cars')
+        .then(cars => {
+            res.status(200).json(cars)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ message: 'Error retrieving cars.' })
+        })
+})
+
+router.post('/', (req, res) => {
+    const data = req.body
+
+    db('cars')
+        .insert(data)
+        .then(cars => {
+            res.status(201).json(cars)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({ message: 'Error storing car data.' })
+        })        
+})
+
+module.exports = router
